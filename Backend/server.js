4c268dc3f16a1842b4f2f6554d66bfd7b652f43e
Isha Agarwal <50719265+Isha77agarwal@users.noreply.github.com>
2022-11-10 22:56:24 +0530

    update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -30,10 +30,16 @@ app.use("/api/workerData", workerFormRoutes);
 app.use("/api/queue", schedulingQueueRoutes);
 
 sendEmailOnStatusChangeWorker.start();
+//for creating production ready react app
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')))
+}else{
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
